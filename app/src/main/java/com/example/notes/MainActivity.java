44992eef692cd0e3dc0bf9119d85519fd3cadc58
alekswonder <keeper.of.something.good@gmail.com>
2022-06-21 19:12:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.notes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewNotes;
+    private final ArrayList<Note> notes = new ArrayList<>();
+    private NotesAdapter adapter;
+    private MainViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+        adapter = new NotesAdapter(notes);
+        recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
+        getData();
+        recyclerViewNotes.setAdapter(adapter);
+        adapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
+            @Override
+            public void onNoteClick(int position) {
+                Toast.makeText(MainActivity.this, "Номер позиции: " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongNoteClick(int position) {
+                removeNote(position);
+            }
+        });
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                removeNote(viewHolder.getAdapterPosition());
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerViewNotes);
+    }
+
+    private void removeNote(int position) {
+        Note note = adapter.getNotes().get(position);
+        viewModel.deleteNote(note);
+    }
+
+
+    public void onClickAddNote(View view) {
+        Intent intent = new Intent(this, AddNoteActivity.class);
+        startActivity(intent);
+    }
+
+    private void getData() {
+        LiveData<List<Note>> notesFromDb = viewModel.getNotes();
+        notesFromDb.observe((LifecycleOwner) this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable @androidx.annotation.Nullable List<Note> notesFromLiveData) {
+                adapter.setNotes(notesFromLiveData);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/notes/Note.java
@@ -0,0 +1,94 @@
+package com.example.notes;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "notes")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String description;
+    private int dayOfWeek;
+    private int priority;
+
+    public Note(int id, String title, String description, int dayOfWeek, int priority) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dayOfWeek = dayOfWeek;
+        this.priority = priority;
+    }
+
+    @Ignore
+    public Note(String title, String description, int dayOfWeek, int priority) {
+        this.title = title;
+        this.description = description;
+        this.dayOfWeek = dayOfWeek;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDayOfWeek(int dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public static String getDayAsString(int position) {
+        switch (position) {
+            case 0:
+                return "Понедельник";
+            case 1:
+                return "Вторник";
+            case 2:
+                return "Среда";
+            case 3:
+                return "Четверг";
+            case 4:
+                return "Пятница";
+            case 5:
+                return "Суббота";
+            case 6:
+                return "Воскресенье";
+            default:
+                return "Нет дня недели";
+        }
+    }
+
+
+}
